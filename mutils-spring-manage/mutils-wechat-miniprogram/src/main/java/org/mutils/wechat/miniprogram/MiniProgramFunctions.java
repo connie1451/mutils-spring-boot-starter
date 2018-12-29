f49a41bdbf1f87b3dbe48e3920bb4b21ed59c3a1
mintonzhang <45935465+mintonzhang@users.noreply.github.com>
2018-12-29 17:19:23 +0800

    小程序更新

--- mutils-spring-manage/mutils-wechat-miniprogram/src/main/java/org/mutils/wechat/miniprogram/MiniProgramFunctions.java
+++ mutils-spring-manage/mutils-wechat-miniprogram/src/main/java/org/mutils/wechat/miniprogram/MiniProgramFunctions.java
@@ -1,14 +1,26 @@
 package org.mutils.wechat.miniprogram;
 
+import java.security.AlgorithmParameters;
+import java.security.Security;
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+
 import org.apache.http.HttpEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.util.EntityUtils;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.bouncycastle.util.Arrays;
+import org.bouncycastle.util.encoders.Base64;
 import org.mutils.wechat.miniprogram.model.Code2SessionReturnModel;
+import org.mutils.wechat.miniprogram.model.UserInfoModel;
 
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 
 import cn.minsin.core.exception.MutilsErrorException;
 import cn.minsin.core.init.WechatMiniProgramConfig;
@@ -46,11 +58,55 @@ public class MiniProgramFunctions extends FunctionRule {
 			String string = EntityUtils.toString(entity);
 			response.close();
 			build.close();
-			System.out.println("response= " + string);
+			slog.info("Code2SessionReturnModel string is {}",string);
 	        return JSON.parseObject(string,Code2SessionReturnModel.class);
 		}catch (Exception e) {
 			throw new MutilsErrorException(e, "小程序使用code换取openid等信息失败");
 		}
+	}
+	
+	/**
+	 * 解密用户敏感数据获取用户信息
+	 * 
+	 * @param sessionKey    数据进行加密签名的密钥
+	 * @param encryptedData 包括敏感数据在内的完整用户信息的加密数据
+	 * @param iv            加密算法的初始向量
+	 */
+	public static UserInfoModel getUserInfo(String encryptedData, String code, String iv) {
+		try {
+			Code2SessionReturnModel jscode2session = jscode2session(code);
+			// 被加密的数据
+			byte[] dataByte = Base64.decode(encryptedData);
+			// 加密秘钥
+			byte[] keyByte = Base64.decode(jscode2session.getSession_key());
+			// 偏移量
+			byte[] ivByte = Base64.decode(iv);
+
+			// 如果密钥不足16位，那么就补足. 这个if 中的内容很重要
+			int base = 16;
+			if (keyByte.length % base != 0) {
+				int groups = keyByte.length / base + (keyByte.length % base != 0 ? 1 : 0);
+				byte[] temp = new byte[groups * base];
+				Arrays.fill(temp, (byte) 0);
+				System.arraycopy(keyByte, 0, temp, 0, keyByte.length);
+				keyByte = temp;
+			}
+			// 初始化
+			Security.addProvider(new BouncyCastleProvider());
+			Cipher cipher = Cipher.getInstance("AES/CBC/PKCS7Padding", "BC");
+			SecretKeySpec spec = new SecretKeySpec(keyByte, "AES");
+			AlgorithmParameters parameters = AlgorithmParameters.getInstance("AES");
+			parameters.init(new IvParameterSpec(ivByte));
+			cipher.init(Cipher.DECRYPT_MODE, spec, parameters);// 初始化
+			byte[] resultByte = cipher.doFinal(dataByte);
+			if (null != resultByte && resultByte.length > 0) {
+				String result = new String(resultByte, "UTF-8");
+				return JSONObject.parseObject(result, UserInfoModel.class);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
 	}
 
 }

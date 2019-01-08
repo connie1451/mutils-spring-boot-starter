e30bf4beee3d4b3dea544d06079e18f7c3200da4
mintonzhang <45935465+mintonzhang@users.noreply.github.com>
2019-01-08 10:59:52 +0800

    删除多余文件

--- mutils-spring-manage/mutils-file-server/src/main/java/cn/minsin/file/repository/FileRepository.java
+++
@@ -1,9 +0,0 @@
-package cn.minsin.file.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import cn.minsin.file.model.FileModel;
-
-public interface FileRepository extends MongoRepository<FileModel,String> {
-
-}
